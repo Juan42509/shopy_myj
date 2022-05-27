d16c9db322c591702ff67964f07e7c4d607166ca
LeidyFonseca24 <66701537+LeidyFonseca24@users.noreply.github.com>
2022-05-26 23:34:58 -0500

    Ajuste de diseño formularios y menu

--- views/categoria/crear.php
+++ views/categoria/crear.php
@@ -1,13 +1,27 @@
 <?php include('./views/layout/header.php'); ?>
 <?php include('./views/layout/sidebar.php'); ?>
-
-<h1>Crear nueva categoria</h1>
-
+<!DOCTYPE html>
+<html class="claseimag">
+<head>
+<link rel="stylesheet" href="<?=base_url?>assets/css/formularios.css">
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="<?=base_url?>assets/css/formularios.css">
+</head>
+<body>
+<div class="container" id="registration-form">
+        <div class="image"></div>
+        <div class="frm">
+            <h1>Creación de categorías</h1>
 <form action="<?=base_url?>categoria/save" method="POST">
-	<label for="nombre">Nombre</label>
-	<input type="text" name="nombre" required/>
-	
-	<input type="submit" value="Guardar" />
+<div class="form-group">
+<label for="nombre">Nombre Categoría:</label>
+    <input type="text" name="nombre" placeholder="Ingresar nombre de categoría" class="form-control" id="username" required/>
+ 
+    
+    <button type="submit" class="btn btn-warning btn-lg" id="button-btn" value="guardar">Crear</button>
+    
 </form>
-
 <?php include('./views/layout/footer.php'); ?>
+
--- views/layout/sidebar.php
+++ views/layout/sidebar.php
@@ -26,20 +26,6 @@
 			<h3><?=$_SESSION['identity']->nombre?> <?=$_SESSION['identity']->apellidos?></h3>
 		<?php endif; ?>
 
-		<ul>
-			<?php if(isset($_SESSION['admin'])): ?>
-				<li><a href="<?=base_url?>categoria/index">Gestionar categorias</a></li>
-				<li><a href="<?=base_url?>producto/gestion">Gestionar productos</a></li>
-				<li><a href="<?=base_url?>pedido/gestion">Gestionar pedidos</a></li>
-			<?php endif; ?>
-			
-			<?php if(isset($_SESSION['identity'])): ?>
-				<li><a href="<?=base_url?>pedido/mis_pedidos">Mis pedidos</a></li>
-				<li><a href="<?=base_url?>usuario/logout">Cerrar sesión</a></li>
-			<?php else: ?> 
-				<li><a href="<?=base_url?>usuario/registro">Registrate aqui</a></li>
-			<?php endif; ?> 
-		</ul>
 	</div>
 
 </aside>
